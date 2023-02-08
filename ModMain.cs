6ffc8e49e46c8eab4751ef75e4b580a56406c343
ArchieV1 <archie.j.vann@gmail.com>
2023-02-08 18:58:10 +0000

    Updated to normal code standards Refactored Added LoadPrefabLazy, GetAllAssets, ListAllAssets Updated BuildingCheck Moved SetupHousesParliament from preload Fixed logging to be consistant

--- ModMain.cs
+++ ModMain.cs
@@ -1,5 +1,7 @@
 ﻿using Harmony;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Assets.Code;
 using I2.Loc;
@@ -20,8 +22,7 @@ namespace mods.CrownedRepublic
         public static GameObject HouseOfParliamentPrefab;
 
         public static ModMain Inst;
-
-
+        
         public void Preload(KCModHelper helper)
         {
             Inst = this;
@@ -32,108 +33,175 @@ namespace mods.CrownedRepublic
             try
             {
                 AssetBundle = KCModHelper.LoadAssetBundle(assetBundlePath, "house_of_parliament");
-                Debug.Log(assetBundlePath);
+                Helper.Log($"SUCCESS: Loaded AssetBundle at: {assetBundlePath}");
 
                 if (AssetBundle is null)
                 {
-                    Helper.Log($"Failed to load asset bundle at {assetBundlePath}");
+                    Helper.Log($"FAILURE: Didn't load asset bundle at {assetBundlePath}");
                 }
             }
 
-            catch (Exception) 
+            catch (Exception e) 
             {
-                Helper.Log(Helper.modPath);
+                Helper.Log($"FAILURE: Error occurred: {e}");
             }
 
 
             if (AssetBundle != null)
             {
-
-                //House_of_Parliament
-                HouseOfParliamentPrefab = AssetBundle.LoadAsset("assets/workspace/House_of_Parliament.prefab") as GameObject;
+                Helper.Log(ListAllAssets(AssetBundle, newLine: true));
                 
-                //=========================================================================//
-
-                //attaches the Building script to the prefab
-                HouseOfParliamentPrefab?.AddComponent<Building>();
-                Building bHouseOfParliament = HouseOfParliamentPrefab.GetComponent<Building>();
-
-                BuildingCollider houseOfParliamentCol = bHouseOfParliament.transform.Find("Offset").Find("House_of_Parliament").gameObject.AddComponent<BuildingCollider>();
-                houseOfParliamentCol.Building = bHouseOfParliament;
-
-
-                bHouseOfParliament.UniqueName = "house_of_parliament";
-                bHouseOfParliament.customName = "House of Parliament";
-                bHouseOfParliament.uniqueNameHash = "".GetHashCode();
-
-
-                bHouseOfParliament.JobCategory = JobCategory.Castle;
-                //bHouse_of_Parliament.placementSounds = new string[] { "castleplacement" };//replace with building sound
-                //bHouse_of_Parliament.SelectionSounds = new string[] { "BuildingSelectCastleGate" };//replace with manor door sound
-                bHouseOfParliament.skillUsed = "Castle";
-                bHouseOfParliament.size = new Vector3(6f, 4f, 3f);
-
-                //cost
-                ResourceAmount cost = new ResourceAmount();
-                cost.Set(FreeResourceType.Tree, 750);
-                cost.Set(FreeResourceType.Stone, 800);
-                cost.Set(FreeResourceType.Gold, 9000);
-                cost.Set(FreeResourceType.IronOre, 50);
-                SetField<Building>("Cost", bFlags, bHouseOfParliament, cost);
-
-                //place people
-                bHouseOfParliament.personPositions = new Transform[] { HouseOfParliamentPrefab.transform.Find("Offset").Find("P1") };
-                bHouseOfParliament.DisplayModel = HouseOfParliamentPrefab;
-                bHouseOfParliament.SubStructure = false;
-
-                bHouseOfParliament.WorkersForFullYield = 50;//number of politicians
-                bHouseOfParliament.BuildAllowedWorkers = 40;
-
-                bHouseOfParliament.BuildShaderMinYAdjustment = 0f;
-                bHouseOfParliament.BuildShaderMaxYAdjustment = 3f;
-
-                bHouseOfParliament.Stackable = false;
-                bHouseOfParliament.troopsCanWalkOn = false;
-                bHouseOfParliament.transportCartsCanTravelOn = false;
-                bHouseOfParliament.allowOverAndUnderAqueducts = false;
-                bHouseOfParliament.allowCastleBlocksOnTop = 0;
-                bHouseOfParliament.dragPlacementMode = 0;
-                bHouseOfParliament.doBuildAnimation = true;
-                bHouseOfParliament.MaxLife = 10;
-                bHouseOfParliament.ignoreRoadCoverageForPlacement = true;
-
+                //House_of_Parliament
+                // string houseOfParliamentPrefabLocation = "assets/kcassets/workspace/house_of_parliament.prefab";
+                // HouseOfParliamentPrefab = AssetBundle.LoadAsset(houseOfParliamentPrefabLocation) as GameObject;
+                HouseOfParliamentPrefab = LoadPrefabLazy(AssetBundle, "house_of_parliament.prefab");
+                if (HouseOfParliamentPrefab != null)
+                {
+                    SetupHousesParliament();
+                    
+                    // Only load harmony if prefab(s) have loaded correctly
+                    HarmonyInstance harmony = HarmonyInstance.Create("CrownedRepublic");
+                    harmony.PatchAll(Assembly.GetExecutingAssembly());
+                    Helper.Log("SUCCESS: Loaded HouseOfParliamentPrefab");
+                }
+                else
+                {
+                    Helper.Log("FAILURE: HouseOfParliamentPrefab not loaded (Likely not found)");
+                }
             }
             else
             {
-                Helper.Log("Bundle not loaded");
+                Helper.Log("FAILURE: AssetBundle not loaded");
             }
-
-            //initializing harmony patches
-            var harmony = HarmonyInstance.Create("harmony");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-
-
         }
 
+        public void SetupHousesParliament()
+        {
+            //attaches the Building script to the prefab
+            if (HouseOfParliamentPrefab is null) throw new Exception("HouseOfParliamentPrefab is null!\n" +
+                                                                     "Make sure it is loaded BEFORE running this method");
+            
+            HouseOfParliamentPrefab.AddComponent<Building>();
+            Building bHouseOfParliament = HouseOfParliamentPrefab.GetComponent<Building>();
 
-        /*
+            BuildingCollider houseOfParliamentCol = bHouseOfParliament.transform.Find("Offset")
+                .Find("House_of_Parliament").gameObject.AddComponent<BuildingCollider>();
+            houseOfParliamentCol.Building = bHouseOfParliament;
+
+
+            bHouseOfParliament.UniqueName = "house_of_parliament";
+            bHouseOfParliament.customName = "House of Parliament";
+            bHouseOfParliament.uniqueNameHash = bHouseOfParliament.UniqueName.GetHashCode();
+
+
+            bHouseOfParliament.JobCategory = JobCategory.Castle;
+            //bHouse_of_Parliament.placementSounds = new string[] { "castleplacement" };//replace with building sound
+            //bHouse_of_Parliament.SelectionSounds = new string[] { "BuildingSelectCastleGate" };//replace with manor door sound
+            bHouseOfParliament.skillUsed = "Castle";
+            bHouseOfParliament.size = new Vector3(6f, 4f, 3f);
+
+            // Cost
+            ResourceAmount cost = new ResourceAmount();
+            cost.Set(FreeResourceType.Tree, 750);
+            cost.Set(FreeResourceType.Stone, 800);
+            cost.Set(FreeResourceType.Gold, 9000);
+            cost.Set(FreeResourceType.IronOre, 50);
+            SetField("Cost", bFlags, bHouseOfParliament, cost);
+
+            // Place people
+            bHouseOfParliament.personPositions = new[]
+            {
+                HouseOfParliamentPrefab.transform.Find("Offset").Find("P1")
+            };
+            bHouseOfParliament.DisplayModel = HouseOfParliamentPrefab;
+            bHouseOfParliament.SubStructure = false;
+
+            bHouseOfParliament.WorkersForFullYield = 50; // Number of "Politicians" TODO In game name defined where?
+            bHouseOfParliament.BuildAllowedWorkers = 40;
+
+            bHouseOfParliament.BuildShaderMinYAdjustment = 0f;
+            bHouseOfParliament.BuildShaderMaxYAdjustment = 3f;
+
+            bHouseOfParliament.Stackable = false;
+            bHouseOfParliament.troopsCanWalkOn = false;
+            bHouseOfParliament.transportCartsCanTravelOn = false;
+            bHouseOfParliament.allowOverAndUnderAqueducts = false;
+            bHouseOfParliament.allowCastleBlocksOnTop = 0;
+            bHouseOfParliament.dragPlacementMode = 0;
+            bHouseOfParliament.doBuildAnimation = true;
+            bHouseOfParliament.MaxLife = 10;
+            bHouseOfParliament.ignoreRoadCoverageForPlacement = true;
+            
+            BuildingCheck(bHouseOfParliament);
+        }
+
+        
         public static void BuildingCheck(Building building)
         {
-            string bInfo = "Job Category: " + building.JobCategory.ToString() +
-                "\nPlacement Sound: " + building.placementSounds.ToString() +
-                "\nSelection Sound: " + building.SelectionSounds.ToString() +
-                "\nSelected Ambient Sound: " + building.selectedAmbientSound.ToString()
-                "\nUnique Name: " + building.UniqueName.ToString() +
-                "\nCategory Name: " + building.CategoryName.ToString() +
-                "\nDisplay Model: " + building.DisplayModel.ToString() +
-                "\nSubstructure: " + building.SubStructure.ToString() +
-                "\nSkill Used: " + building.skillUsed.ToString() +
-                "\nSize: " + building.size.ToString() +
-                "\nCost: " + building.GetCost().ToString() +
-                "\n");
-            helper.Log(bInfo);
+            // string bInfo = "Job Category: " + building.JobCategory.ToString() +
+            //     "\nPlacement Sound: " + building.placementSounds.ToString() +
+            //     "\nSelection Sound: " + building.SelectionSounds.ToString() +
+            //     "\nSelected Ambient Sound: " + building.selectedAmbientSound.ToString() +
+            //     "\nUnique Name: " + building.UniqueName.ToString() +
+            //     "\nCategory Name: " + building.CategoryName.ToString() +
+            //     "\nDisplay Model: " + building.DisplayModel.ToString() +
+            //     "\nSubstructure: " + building.SubStructure.ToString() +
+            //     "\nSkill Used: " + building.skillUsed.ToString() +
+            //     "\nSize: " + building.size.ToString() +
+            //     "\nCost: " + building.GetCost().ToString() +
+            //     "\n");
+
+            string bInfo = $"UniqName:   {building.UniqueName}\n" +
+                           $"DisplayNm:  {building.DisplayModel}\n" +
+                           $"JobCat:     {building.JobCategory}\n" +
+                           $"PlaceSound: {building.placementSounds}\n" +
+                           $"SelecSound: {building.SelectionSounds}\n" +
+                           $"CatName:    {building.CategoryName}\n" +
+                           $"Substrucut: {building.SubStructure}\n" +
+                           $"SkillUsed:  {building.skillUsed}\n" +
+                           $"Size:       {building.size}\n" +
+                           $"Cost:       {building.GetCost(Player.inst.PlayerLandmassOwner)}";
+            
+            Helper.Log(bInfo);
         }
-        */
+
+        /// <summary>
+        /// Returns string of all Assets in given AssetBundle
+        /// </summary>
+        /// <param name="assetBundle"></param>
+        /// <param name="delimiter"></param>
+        /// <param name="newLine"></param>
+        /// <returns></returns>
+        public string ListAllAssets(AssetBundle assetBundle, string delimiter=" | ", bool newLine=false)
+        {
+            List<string> assets = GetAllAssets(assetBundle);
+
+            return assets.Join(delimiter: !newLine ? delimiter : "\n");
+        }
+
+        public static List<string> GetAllAssets(AssetBundle assetBundle)
+        {
+            return assetBundle.GetAllAssetNames().ToList();
+        }
+
+        /// <summary>
+        /// Loads prefab from any location inside an AssetBundle. Just requires the name of the prefab (Or most of it)
+        /// </summary>
+        /// <param name="assetBundle"></param>
+        /// <param name="prefabName">.prefab is optional</param>
+        /// <returns></returns>
+        public static GameObject LoadPrefabLazy(AssetBundle assetBundle, string prefabName)
+        {
+            foreach (string assetName in GetAllAssets(assetBundle))
+            {
+                if (assetName.Contains(prefabName))
+                {
+                    return assetBundle.LoadAsset(assetName) as GameObject;
+                }
+            }
+
+            throw new Exception($"{prefabName} was not found inside {assetBundle}");
+        }
 
 
         BindingFlags bFlags = BindingFlags.NonPublic | BindingFlags.Instance;
